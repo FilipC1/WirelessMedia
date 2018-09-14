8f17605efe812d6e6d271dee76d24a4dbb439033
FilipC1 <32641597+FilipC1@users.noreply.github.com>
2018-09-14 09:19:55 +0200

    Pravljenje baze; Pravljenje Model klasa
    Pravljenje baze podataka i ubacivanje query-ja;
    Pravljenje konekcije sa bazom podataka;
    Pravljenje Model kalsa - Proizvod i WirelessMediaContext;
    Registrovanje DB Context-a u servisima;

---
+++ WirelessMedia/WirelessMedia/Models/Proizvod.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WirelessMedia.Models
+{
+    [Table("Proizvod")]
+    public partial class Proizvod
+    {
+        public int ProizvodId { get; set; }
+        [StringLength(100)]
+        public string Naziv { get; set; }
+        [StringLength(100)]
+        public string Opis { get; set; }
+        [StringLength(100)]
+        public string Kategorija { get; set; }
+        [StringLength(100)]
+        public string Proizvodjac { get; set; }
+        [StringLength(100)]
+        public string Dobavljac { get; set; }
+        [Column(TypeName = "decimal(10, 2)")]
+        public decimal Cena { get; set; }
+    }
+}
---
+++ WirelessMedia/WirelessMedia/Models/WirelessMediaContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace WirelessMedia.Models
+{
+    public partial class WirelessMediaContext : DbContext
+    {
+        
+        public WirelessMediaContext(DbContextOptions<WirelessMediaContext> opcije)
+            : base(opcije)
+        {
+        }
+
+        public virtual DbSet<Proizvod> Proizvodi { get; set; }
+
+        
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {}
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
